a820c534d2106991a94a1f1fb6517ee771e367f7
Yaoyuan Chu <chuyaoyuan@gmail.com>
2022-07-14 21:57:38 +0800

    修改

--- src/main/java/com/yaoyuan/jiscuss/util/DelTagsUtil.java
+++ src/main/java/com/yaoyuan/jiscuss/util/DelTagsUtil.java
@@ -2,14 +2,10 @@ package com.yaoyuan.jiscuss.util;
 
 /**
  * @author yaoyuan2.chu
- * @Title:
+ * @Title: 去除内容页代码里的HTML标签
  * @Package com.yaoyuan.jiscuss.util
  * @Description:
  * @date 2020/8/31 20:52
- */
-
-/**
- * 去除内容页代码里的HTML标签
  */
 public class DelTagsUtil {
     /**
@@ -54,8 +50,7 @@ public class DelTagsUtil {
     }
 
     public static void main(String[] args) {
-        String htmlStr = "<script type>var i=1; alert(i)</script><style> .font1{font-size:12px}</style><span>少年中国说。</span>红日初升，其道大光。<h3>河出伏流，一泻汪洋。</h3>潜龙腾渊， 鳞爪飞扬。乳 虎啸  谷，百兽震惶。鹰隼试翼，风尘吸张。奇花初胎，矞矞皇皇。干将发硎，有作其芒。天戴其苍，地履其黄。纵有千古，横有" +
-                "八荒。<a href=\"www.baidu.com\">前途似海，来日方长</a>。<h1>美哉我少年中国，与天不老！</h1><p>壮哉我中国少年，与国无疆！</p>";
+        String htmlStr = "test";
         System.out.println(getTextFromHtml(htmlStr));
     }
 
