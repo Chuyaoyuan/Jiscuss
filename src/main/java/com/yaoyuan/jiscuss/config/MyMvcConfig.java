136963e2062ef34d3ab1bf06a2fdfed812af5ab0
Yaoyuan Chu <chuyaoyuan@gmail.com>
2022-08-30 00:38:18 +0800

    修改

--- src/main/java/com/yaoyuan/jiscuss/config/MyMvcConfig.java
+++ src/main/java/com/yaoyuan/jiscuss/config/MyMvcConfig.java
@@ -7,16 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-
+    /*
+    * addResourceHandlers
+    * void
+    */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
-
     }
 
 }
