11ef997085a91f3043980181fcdc19533563277f
Yaoyuan Chu <chuyaoyuan@gmail.com>
2023-08-07 20:22:24 +0800

    health & log
    health & log

--- src/main/java/com/jiscuss/JiscussApp.java
+++ src/main/java/com/jiscuss/JiscussApp.java
@@ -1,10 +1,12 @@
 package com.jiscuss;
 
 import org.noear.solon.Solon;
+import org.noear.solon.annotation.SolonMain;
 
 /**
  * @author cyy 2023/7/12 created
  */
+@SolonMain
 public class JiscussApp {
     public static void main(String[] args) {
         Solon.start(JiscussApp.class, args)
--- src/main/java/com/jiscuss/controller/DemoController.java
+++ src/main/java/com/jiscuss/controller/DemoController.java
@@ -3,14 +3,21 @@ package com.jiscuss.controller;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.core.handle.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * @author cyy 2023/7/12 created
  */
 @Controller
 public class DemoController {
+
+    static Logger log = LoggerFactory.getLogger(DemoController.class);
+
     @Mapping("/")
     public Object test(){
+        log.info("test");
         ModelAndView model = new ModelAndView("index.htm");
         model.put("title","jiscuss");
         model.put("message","你好 jiscuss!");
